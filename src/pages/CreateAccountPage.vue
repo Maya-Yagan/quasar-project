24e143cca96f9ff5795d6e349e232df6eb10608a
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:35:04 +0300

    A page for creating user account was added

---
+++ src/pages/CreateAccountPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <create-account></create-account>
+</template>
+
+<script lang="ts">
+import CreateAccount from 'src/components/CreateAccount.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    CreateAccount,
+  },
+});
+</script>
