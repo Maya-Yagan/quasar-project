c2b7703a673498cd12c720e8d3c4907dadf7fe66
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:36:47 +0300

    A page for logging in was added

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <login-section></login-section>
+</template>
+
+<script lang="ts">
+import LoginSection from 'src/components/LoginSection.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    LoginSection,
+  },
+});
+</script>
