e5a875d889e83df22367651fc5bc1e8cb76c2d51
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:45:29 +0300

    The new pages were added

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,20 +4,30 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/departments',
+    component: () => import('layouts/DepartmentsLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'page2', component: () => import('pages/PageTwo.vue') },
+      { path: '', component: () => import('pages/AllDepartments.vue') },
     ],
   },
   {
     path: '/login',
     component: () => import('layouts/LoginLayout.vue'),
     children: [
-      { path: '', component: () => import('components/LoginSection.vue') },
+      { path: '', component: () => import('pages/LoginPage.vue') },
       {
         path: 'createAccount/:email',
         name: 'createAccount',
-        component: () => import('components/CreateAccount.vue'),
+        component: () => import('pages/CreateAccountPage.vue'),
+        props: true,
+      },
+      {
+        path: 'signIn/:email',
+        name: 'signIn',
+        component: () => import('pages/SignInPage.vue'),
         props: true,
       },
     ],
@@ -25,8 +35,13 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/cart',
     component: () => import('layouts/CartLayout.vue'),
+    children: [{ path: '', component: () => import('pages/CartPage.vue') }],
+  },
+  {
+    path: '/userAccount',
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('components/ShoppingCart.vue') },
+      { path: '', component: () => import('pages/UserAccountPage.vue') },
     ],
   },
   {
