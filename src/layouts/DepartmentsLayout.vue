7e783b6865f45133be1c922cad1f2d3fff8550f8
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:31:21 +0300

    New layout was added

---
+++ src/layouts/DepartmentsLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hHh Lpr lff">
+    <nav-bar></nav-bar>
+    <q-footer>
+      <main-footer></main-footer>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import NavBar from 'src/components/NavBar.vue';
+import MainFooter from 'src/components/MainFooter.vue';
+export default {
+  name: 'CartLayout',
+  components: {
+    NavBar,
+    MainFooter,
+  },
+};
+</script>
