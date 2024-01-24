73e2db137a6945b4dca98b825bebc3d24f896e9f
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:27:53 +0300

    New changes

--- src/components/ProductsBrowser.vue
+++ src/components/ProductsBrowser.vue
@@ -7,6 +7,8 @@
         :imgLink="product.imgLink"
         :price="product.price"
         :title="product.title"
+        :companyName="product.companyName"
+        :productInfo="product.productInfo"
         :currentPage="currentPage"
         @addToCart="handleAddToCart(product)"
         class="product-card"
@@ -35,203 +37,96 @@
 </template>
 
 <script lang="ts">
+import { useUserStore } from 'src/stores/user';
 import ProductCard from '../components/ProductCard.vue';
 import { useCartStore } from 'src/stores/cart';
+import { computed, onMounted, ref, toRefs } from 'vue';
+import { useAxios } from 'src/services/useAxios';
+import { useRouter } from 'vue-router';
 interface Product {
   title: string;
   imgLink: string;
   price: number;
+  companyName: string;
+  productInfo: string;
+  quantity: number;
 }
 export default {
-  components: {
-    ProductCard,
+  props: {
+    itemsPerPage: {
+      type: Number,
+      default: 6, // Default value if not provided
+    },
   },
-  data() {
+  setup(props) {
+    const router = useRouter();
+    const userStore = useUserStore();
+    const { isSignedIn } = toRefs(userStore);
+    const { loading, get } = useAxios();
+    const currentPage = ref(1);
+    // Ref to store the fetched product data
+    const products = ref<Product[]>([]);
+    // Fetch product data from Express API when component is mounted
+    onMounted(async () => {
+      try {
+        const response = await get<Product[]>('/products');
+        // Update with API endpoint
+        console.log('Product data:', response.data);
+        products.value = response.data;
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
+    });
+    const showPreviousButton = computed(() => currentPage.value > 1);
+    const showNextButton = computed(() => {
+      const totalPages = Math.ceil(products.value.length / props.itemsPerPage);
+      return currentPage.value < totalPages;
+    });
+    const visibleProducts = computed(() => {
+      const startIndex = (currentPage.value - 1) * props.itemsPerPage;
+      const endIndex = Math.min(
+        startIndex + props.itemsPerPage,
+        products.value.length
+      );
+      return products.value.slice(startIndex, endIndex);
+    });
+    const handleAddToCart = (product: Product) => {
+      if (isSignedIn.value) {
+        useCartStore().addToCart({
+          product: { ...product },
+          page: currentPage.value,
+        });
+      } else {
+        router.push('/login');
+      }
+    };
+    const goToPreviousPage = () => {
+      if (currentPage.value > 1) {
+        currentPage.value--;
+      }
+    };
+
+    const goToNextPage = () => {
+      if (showNextButton.value) {
+        currentPage.value++;
+      }
+    };
     return {
-      itemsPerPage: 6,
-      currentPage: 1,
-      products: [
-        {
-          imgLink:
-            'https://i5.walmartimages.com/seo/Chaps-Boys-Crewneck-Sweater-with-Long-Sleeves-Sizes-4-18_8ee98f21-87fd-4736-bb34-f001f0e12304.3160b22e6022b3f519be538a824346d9.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 29.5,
-          title: 'Title 1',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 2',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 3',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 4',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 5',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 6',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 7',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/seo/Chaps-Boys-Crewneck-Sweater-with-Long-Sleeves-Sizes-4-18_8ee98f21-87fd-4736-bb34-f001f0e12304.3160b22e6022b3f519be538a824346d9.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 29.5,
-          title: 'Title 8',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/seo/Chaps-Boys-Crewneck-Sweater-with-Long-Sleeves-Sizes-4-18_8ee98f21-87fd-4736-bb34-f001f0e12304.3160b22e6022b3f519be538a824346d9.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 29.5,
-          title: 'Title 9',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 10',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 11',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 12',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/seo/Chaps-Boys-Crewneck-Sweater-with-Long-Sleeves-Sizes-4-18_8ee98f21-87fd-4736-bb34-f001f0e12304.3160b22e6022b3f519be538a824346d9.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 29.5,
-          title: 'Title 13',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 14',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 15',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 16',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 17',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 18',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 19',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/seo/Chaps-Boys-Crewneck-Sweater-with-Long-Sleeves-Sizes-4-18_8ee98f21-87fd-4736-bb34-f001f0e12304.3160b22e6022b3f519be538a824346d9.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 29.5,
-          title: 'Title 20',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/seo/Chaps-Boys-Crewneck-Sweater-with-Long-Sleeves-Sizes-4-18_8ee98f21-87fd-4736-bb34-f001f0e12304.3160b22e6022b3f519be538a824346d9.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 29.5,
-          title: 'Title 21',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 22',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/c249b58e-e499-4e86-8846-b45bd4d42656.96b4164afd60e863ecfe440373ff72d0.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 19.99,
-          title: 'Title 23',
-        },
-        {
-          imgLink:
-            'https://i5.walmartimages.com/asr/07ac91bd-de83-423d-b478-b6e8d5f83a1c.677093a85ec4621848e2809493c6edde.jpeg?odnHeight=350&odnWidth=350&odnBg=FFFFFF',
-          price: 9.99,
-          title: 'Title 24',
-        },
-      ],
+      isSignedIn,
+      userStore,
+      products,
+      loading,
+      currentPage,
+      visibleProducts,
+      showPreviousButton,
+      showNextButton,
+      handleAddToCart,
+      goToPreviousPage,
+      goToNextPage,
     };
   },
-  computed: {
-    visibleProducts() {
-      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      const endIndex = Math.min(
-        startIndex + this.itemsPerPage,
-        this.products.length
-      );
-      return this.products.slice(startIndex, endIndex);
-    },
-    showPreviousButton() {
-      return this.currentPage > 1;
-    },
-    showNextButton() {
-      const totalPages = Math.ceil(this.products.length / this.itemsPerPage);
-      return this.currentPage < totalPages;
-    },
-  },
-  methods: {
-    handleAddToCart(product: Product) {
-      useCartStore().addToCart({
-        product: { ...product },
-        page: this.currentPage,
-      });
-    },
-    goToPreviousPage() {
-      if (this.currentPage > 1) {
-        this.currentPage--;
-      }
-    },
-    goToNextPage() {
-      if (this.showNextButton) {
-        this.currentPage++;
-      }
-    },
+  components: {
+    ProductCard,
   },
 };
 </script>
