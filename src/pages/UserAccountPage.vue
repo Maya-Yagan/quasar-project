9213e004ebd44f59eb206980bdeb947910375fc0
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:37:59 +0300

    A page for the user account in was added

---
+++ src/pages/UserAccountPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <user-account></user-account>
+  </q-page>
+</template>
+
+<script lang="ts">
+import UserAccount from 'src/components/UserAccount.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    UserAccount,
+  },
+});
+</script>
