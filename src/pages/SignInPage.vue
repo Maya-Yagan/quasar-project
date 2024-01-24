c9a87ee77ff3e3507545ab65ba3024c0a5304b6a
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:37:25 +0300

    A page for signing in was added

---
+++ src/pages/SignInPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <sign-in></sign-in>
+</template>
+
+<script lang="ts">
+import SignIn from 'src/components/SignIn.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    SignIn,
+  },
+});
+</script>
