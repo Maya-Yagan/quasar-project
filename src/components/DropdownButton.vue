025a3b8d1c4eee76f68ef8dd5625ab29b95ca4aa
Maya-Yagan <maya2002yagan@gmail.com>
2024-01-24 06:21:34 +0300

    New changes

--- src/components/DropdownButton.vue
+++ src/components/DropdownButton.vue
@@ -4,7 +4,7 @@
     flat
     icon="grid_view"
     class="text-capitalize MyBtn"
-    :label="label"
+    :label="props.label"
   >
     <q-menu
       anchor="bottom start"
@@ -15,17 +15,20 @@
       <q-item class="q-pa-none">
         <q-card-section class="scroll-container q-pa-none">
           <dropdown-menu
-            :tabs="tabs"
+            :tabs="props.tabs"
             @tab-hover="handleTabHover"
           ></dropdown-menu>
         </q-card-section>
-        <q-card class="bg-blue-1 row scroll-container" style="width:400px" flat>
+        <q-card
+          class="bg-blue-1 row scroll-container"
+          style="width: 400px"
+          flat
+        >
           <q-card-section v-for="link in currentMiddleLinks" :key="link">
-            <q-item >
-              <a :href="link">{{link}}</a>
+            <q-item>
+              <a :href="link">{{ link }}</a>
             </q-item>
           </q-card-section>
-
         </q-card>
         <dropdown-info
           imgLink="https://i5.walmartimages.com/dfw/4ff9c6c9-5977/k2-_e9468413-fe29-4fb3-8ff5-aab0f25d4023.v1.jpg?odnHeight=142px&odnWidth=142px&odnBg=FFFFFF"
@@ -37,49 +40,38 @@
   ></q-btn>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import DropdownMenu from '../components/DropdownMenu.vue';
 import DropdownInfo from '../components/DropdownInfo.vue';
+import { reactive, ref } from 'vue';
 
-export default {
-  components: {
-    DropdownMenu,
-    DropdownInfo,
-  },
-  props: {
-    label: { type: String },
-    tabs: { type: Array as () => { id: number; label: string }[] },
-  },
-  data() {
-    return {
-      currentMiddleLinks: [] as string[],
-      currentInfo: {
-        imgLink: '',
-        title: '',
-        info: '',
-      },
-    };
-  },
-  methods: {
-    handleTabHover(tabId: number) {
-      this.currentInfo = this.generateInfoForTab(tabId);
-      this.currentMiddleLinks = this.generateLinksForTab(tabId)
-    },
-    generateLinksForTab(tabId: number) {
-      return Array.from(
-        { length: 10 },
-        (_, index) => `Link ${index + 1} for Tab ${tabId}`
-      );
-    },
-    generateInfoForTab(tabId: number) {
-      return {
-        imgLink:
-          'https://i5.walmartimages.com/dfw/4ff9c6c9-5977/k2-_e9468413-fe29-4fb3-8ff5-aab0f25d4023.v1.jpg?odnHeight=142px&odnWidth=142px&odnBg=FFFFFF',
-        title: `Title for Tab ${tabId}`,
-        info: `Info for Tab ${tabId}`,
-      };
-    },
-  },
+const props = defineProps(['label', 'tabs']);
+const currentMiddleLinks = ref([] as string[]);
+const currentInfo = ref({ imgLink: '', title: '', info: '' });
+const state = reactive({
+  currentMiddleLinks,
+  currentInfo,
+});
+
+const handleTabHover = (tabId: number) => {
+  state.currentInfo = generateInfoForTab(tabId);
+  state.currentMiddleLinks = generateLinksForTab(tabId);
+};
+
+const generateLinksForTab = (tabId: number) => {
+  return Array.from(
+    { length: 10 },
+    (_, index) => `Link ${index + 1} for Tab ${tabId}`
+  );
+};
+
+const generateInfoForTab = (tabId: number) => {
+  return {
+    imgLink:
+      'https://i5.walmartimages.com/dfw/4ff9c6c9-5977/k2-_e9468413-fe29-4fb3-8ff5-aab0f25d4023.v1.jpg?odnHeight=142px&odnWidth=142px&odnBg=FFFFFF',
+    title: `Title for Tab ${tabId}`,
+    info: `Info for Tab ${tabId}`,
+  };
 };
 </script>
 <style scoped>
